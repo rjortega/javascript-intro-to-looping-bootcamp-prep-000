fcc81b502bfbe77be9a16eacdc3d58eb3a4904f4
Ricardo Ortega <rikrdo.ortega@gmail.com>
2019-04-15 00:29:07 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,7 @@
 function forLoop(array){
   for (var i = 0; i < 25; i++){
     if (i === 1){
+<<<<<<< HEAD
       array.push("I am 1 strange loop.")
     } else {
       array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`)
@@ -25,4 +26,11 @@ function doWhileLoop(num){
   do {
     console.log("I run once regardless.")
   } while (incrementVariable() < num)
+=======
+      array.push(`I am 1 strange loop.`)
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+>>>>>>> 41d8a1d278ebdbc4b41fde0311944c50deeff953
 }
